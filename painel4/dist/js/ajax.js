07b42ddbb5127a77e0f1267e2b8e241275cb983c
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-17 13:07:42 -0300

    dist

--- painel4/dist/js/ajax.js
+++ painel4/dist/js/ajax.js
@@ -1,14 +1,1 @@
-function chamadaAjax(urldoSelect, funcao) {
-    if (typeof urldoSelect == 'string' && typeof funcao == 'function') {
-        $.ajax({
-            dataType: "json", // tipo de arquivo
-            url: urldoSelect,// local do json
-            data: 'linha', // linha
-            success: function (data) {//se funcionar execulta essa função
-                funcao(data);
-            }//fim funcao
-        });//fim do ajax
-    } else {
-        console.log('Erro de paramentro')
-    }
-}
+function chamadaAjax(a,n){"string"==typeof a&&"function"==typeof n?$.ajax({dataType:"json",url:a,data:"linha",success:function(a){n(a)}}):console.log("Erro de paramentro")}
